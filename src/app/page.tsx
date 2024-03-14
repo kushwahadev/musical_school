4505f8b13b8ca87c38db09dfdac6509154024aea
Dev Kushwaha <devkushwaha3575@gmail.com>
2024-03-14 13:45:19 +0530

    add wavy background and toltip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import FeaturedCourses from "@/components/FeaturedCourses";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import MusicSchoolTestimonialCards from "@/components/TestimonialCards";
 import UpcommingWebinars from "@/components/UpcommingWebinars";
+import Instructors from "@/components/Instructors";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <WhyChooseUs />
       <MusicSchoolTestimonialCards />
       <UpcommingWebinars />
+      <Instructors />
     </main>
   );
 }

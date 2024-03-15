d1690016198839188b89dfb6046c2de4def50947
Dev Kushwaha <devkushwaha3575@gmail.com>
2024-03-15 23:35:41 +0530

    text update

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,7 +6,11 @@ import { BackgroundBeams } from "@/components/ui/background-beams";
 function page() {
   return (
     <div className="flex flex-auto justify-center mt-32 ">
-      <h2>Contact</h2>
+      <h2 className="rounded text-6xl font-bold">Contact</h2>
+      <p>
+        We are here to help any question about our courses, programs, or events.
+        Reach out and let us know how we can assist you in your musical journey
+      </p>
       <BackgroundBeams />
     </div>
   );
